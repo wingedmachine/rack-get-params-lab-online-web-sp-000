866573fea01f12c8406073ccce4fb9e1ebaf04d4
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 21:10:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,9 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
