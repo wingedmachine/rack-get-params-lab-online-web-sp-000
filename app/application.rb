b6299ded5295eefb2b883a553bc3452c4a4ffe0b
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 21:15:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
       item_to_add = req.params["item"]
       if @@items.include?(item_to_add)
         @@cart << item_to_add
-        resp.write "added #{item}"
+        resp.write "added #{item_to_add}"
       else
         resp.write "We don't have that item"
       end
